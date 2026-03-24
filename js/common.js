fc2eb1e65c506ff4badbede8488bdab537622cfa
MINJI, JUNG <105187829+alwaysgoodjmj@users.noreply.github.com>
2026-03-24 17:30:43 +0900

    Add files via upload
    js 추가

--- js/common.js
+++ js/common.js
@@ -1,17 +1,50 @@
-// 메인 모달
+
 document.addEventListener('DOMContentLoaded', () => {
+    const dropDownItems = document.querySelectorAll('.js-dropdown');
+    const btnQuick = document.querySelector('.btn-quick');
+    const btnTrigger = document.querySelector('.btn-quick > a');
     const btnSearch = document.querySelector('.btn-search');
-    const searchPopup = document.querySelector('.search-container');
+    const searchModal = document.querySelector('.search-container');
     const btnClose = document.querySelector('.btn-close');
 
-    if (btnSearch && searchPopup && btnClose) {
+    // gnb
+    dropDownItems.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            dropDownItems.forEach(el => el.classList.remove('ON'));
+            item.classList.add('ON');
+        });
+        item.addEventListener('mouseleave', () => {
+            item.classList.remove('ON');
+        });
+    });
+
+    // quick-menu
+    if (btnQuick && btnTrigger) {
+        btnTrigger.addEventListener('click', (e) => {
+            if (btnQuick.classList.contains('Active')) {
+                btnQuick.classList.toggle('Active');
+            } else {
+                e.preventDefault();
+                btnQuick.classList.add('Active');
+            }
+        });
+    
+        const subLink = btnQuick.querySelectorAll('.quick-sub > a');
+        subLink.forEach(link => {
+            link.addEventListener('click', (e) => {
+                e.stopPropagation();
+            });
+        });
+    }
+
+    // 메인 검색 모달
+    if (btnSearch && searchModal && btnClose) {
         const openSearch = (e) => {
             e.preventDefault();
-            searchPopup.classList.add('ON');
+            searchModal.classList.add('ON');
         };
-
         const closeSearch = () => {
-            searchPopup.classList.remove('ON');
+            searchModal.classList.remove('ON');
         };
 
         btnSearch.addEventListener('click', openSearch);
--- js/swiper.js
+++ js/swiper.js
@@ -1,35 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.slide-container', {
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-          },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+      loop: true,
+      autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      allowTouchMove: false,
     });
 
     const btnPlay = document.querySelector('.btn-play');
     const btnStop = document.querySelector('.btn-pause');
     
     btnStop.addEventListener('click', () => {
-        swiper.autoplay.stop();
-
-        btnStop.classList.add('on');
-        btnPlay.classList.remove('on');
+      swiper.autoplay.stop();
+      btnStop.style.display = 'none';
+      btnPlay.style.display = 'block';
     });
-    
-    btnPlay.addEventListener('click', () => {
-        swiper.autoplay.start();
 
-        btnPlay.classList.add('on');
-        btnStop.classList.remove('on');
+    btnPlay.addEventListener('click', () => {
+      swiper.autoplay.start();
+      btnPlay.style.display = 'none';
+      btnStop.style.display = 'block';
     });
 }); 
   
